02a6a0b314f69459984ff10fbb4ab0576aa2ce46
fybits <sergeig1313@gmail.com>
2023-11-27 20:28:04 +0300

    Add telegram webhook

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const axios = require('axios').default;
 require('dotenv').config()
+const port = process.env.PORT;
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    const response = await axios.get(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/getMe`);
+    console.log(res.data);
+    res.send('Hello World!')
+})
+
+app.post('/webhook', (req, res) => {
+    console.log(req.body);
     res.send('Hello World!')
 })
 
 app.listen(port, async () => {
     console.log(`Example app listening on port ${port}`)
-    const res = await axios.get(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/getMe`);
-    console.log(res.data);
-    // const res2 = await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/setWebhook`, {
-
-    // });
-
+    const res2 = await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/setWebhook`, {
+        url: "https://stock-telegram-bot-production.up.railway.app/webhook"
+    });
+    console.log(res2)
 })
